eaec404e8a311f284b2c176389675f9d8294db2c
Raph <raphael.reynaldo610@gmail.com>
2025-10-10 16:12:51 +0700

    Create ProblemD_Selection.c

---
+++ ProblemD_Selection.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    long long a,b,c,d,e,f;
+    
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d); getchar();
+    
+    e = a * b;
+    f = c + d;
+    
+    if (e < f){
+        printf("True\n");
+    }
+    else{
+        printf("False\n");
+    }
+
+    return 0;
+}
